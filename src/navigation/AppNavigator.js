8044b3ba7f9fc51a0e4f4f54a8efe3a06ef5c31b
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-05-27 20:44:36 -0300

    Add AppNavigator with Login and EnvioHoras screens

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,1 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/login';
+import EnvioHoras2Screen from '../screens/envioHoras2';
 
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="EnvioHoras" component={EnvioHoras2Screen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
