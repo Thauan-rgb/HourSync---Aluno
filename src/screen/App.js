445e4a6f5d78afda54f5b5d40849dcbdce5c17ed
Arthur Vinicius <arthurviniciusns02@gmail.com>
2026-05-27 17:54:21 -0300

    Add files via upload

---
+++ src/screen/App.js
@@ -0,0 +1,24 @@
+import { View, StyleSheet, ScrollView } from 'react-native';
+import Header from './components/Header';        
+import SubmitButton from './components/SubmitButton';
+import ReviewBanner from './components/ReviewBanner';
+import ActivityCard from './components/ActivityCard';
+import styles from './AppStyles';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+
+      <View style={styles.contentWrapper}>
+        <ScrollView style={styles.content}>
+          <ReviewBanner />
+          <ActivityCard />
+        </ScrollView>
+      </View>
+
+      <SubmitButton />
+    </View>
+  );
+}
+
