0a17c9a2b8dcd748bfdfee257e3f5f1e0ed7db4d
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-05-28 17:58:44 -0300

    Update import for styles in envioHoras1.js

--- src/screen/envioHoras1.js
+++ src/screen/envioHoras1.js
@@ -2,7 +2,7 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 import Header from './componentes/headerEnvioHoras1';        // ← header 
 import Formulario from './componentes/formularioEnvioHoras1'; // ← formularios
 import Botao from './componentes/botao';
-import styles from './AppStyles'; // estilo do fundo do card/tela
+import styles from './envioHorasStyles'; // estilo do fundo do card/tela
 
 export default function App() {
   return (
