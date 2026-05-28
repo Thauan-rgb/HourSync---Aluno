92eae154697cea570295cb71514843cf194925ab
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-05-27 21:19:57 -0300

    Add EnvioHoras1Screen and update navigation stack

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screen/login';
+import EnvioHoras1Screen from '../screen/envioHoras1';
 import EnvioHoras2Screen from '../screen/envioHoras2';
 
 const Stack = createNativeStackNavigator();
@@ -10,7 +11,8 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="EnvioHoras" component={EnvioHoras2Screen} />
+        <Stack.Screen name="EnvioHoras1" component={EnvioHoras1Screen} />
+        <Stack.Screen name="EnvioHoras2" component={EnvioHoras2Screen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
