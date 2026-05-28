e3cb18587efee941c547b315fe970a792d67a838
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-05-28 18:08:04 -0300

    Add EsqueceuSenhaScreen to AppNavigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screen/login';
 import EnvioHoras1Screen from '../screen/envioHoras1';
 import EnvioHoras2Screen from '../screen/envioHoras2';
+import EsqueceuSenhaScreen from '../screen/EsqueceuSenha';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="EsqueceuSenha" component={EsqueceuSenhaScreen} />
         <Stack.Screen name="EnvioHoras1" component={EnvioHoras1Screen} />
         <Stack.Screen name="EnvioHoras2" component={EnvioHoras2Screen} />
       </Stack.Navigator>
