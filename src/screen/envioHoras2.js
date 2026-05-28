12297ac48136ad7329898ec69622f1a3daba1454
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-05-28 17:58:56 -0300

    Change styles import to envioHorasStyles
    Updated import statement for styles to use envioHorasStyles.

--- src/screen/envioHoras2.js
+++ src/screen/envioHoras2.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import SubmitButton from '../components/SubmitButton';
 import ReviewBanner from '../components/ReviewBanner';
 import ActivityCard from '../components/ActivityCard';
-import styles from '../AppStyles';
+import styles from './envioHorasStyles'; // estilo do fundo do card/tela
 
 export default function App() {
   return (
