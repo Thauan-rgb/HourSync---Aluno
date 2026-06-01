ffb3f087323dd5659a0c21490848b08a16d25501
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-06-01 19:38:38 -0300

    Add DrawerNavigator for main navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,22 +1,39 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import LoginScreen from '../screen/login';
 import EnvioHoras1Screen from '../screen/envioHoras1';
 import EnvioHoras2Screen from '../screen/envioHoras2';
 import EsqueceuSenhaScreen from '../screen/EsqueceuSenha';
-import NotificacoesScreen from '../screen/notificacoes'; 
+import NotificacoesScreen from '../screen/notificacoes';
+import CustomDrawer from '../components/CustomDrawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawer {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: { width: 280 },
+      }}
+    >
+      <Drawer.Screen name="EnvioHoras1"  component={EnvioHoras1Screen}  />
+      <Drawer.Screen name="EnvioHoras2"  component={EnvioHoras2Screen}  />
+      <Drawer.Screen name="Notificacoes" component={NotificacoesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login"         component={LoginScreen}         />
         <Stack.Screen name="EsqueceuSenha" component={EsqueceuSenhaScreen} />
-        <Stack.Screen name="EnvioHoras1" component={EnvioHoras1Screen} />
-        <Stack.Screen name="EnvioHoras2" component={EnvioHoras2Screen} />
-        <Stack.Screen name="Notificacoes" component={NotificacoesScreen} /> 
+        <Stack.Screen name="Main"          component={DrawerNavigator}     />
       </Stack.Navigator>
     </NavigationContainer>
   );
