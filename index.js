d955ff8b60c1a4737366ebfdd05691796fcf48f7
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-06-01 09:25:26 -0300

    Export auth module from index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+export * from './auth';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
